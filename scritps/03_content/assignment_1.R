11ec4db648e499fac4fcd0e95f905c8da3234e01
caitlincoyle <coyle1122@icloud.com>
2025-09-08 13:34:35 -0400

    Changing quotes ' to backtick `

---
+++ scritps/03_content/assignment_1.R
@@ -0,0 +1,19 @@
+#For Assignment 1
+
+#Load packages
+
+library(tidyverse)
+library(EVR628tools)
+
+#Begin to create plot
+data(data_heatwaves)
+
+#Create a simple plot
+p <- ggplot(data_heatwaves,
+            aes (x = year , y = temp_mean))+
+  geom_point()
+
+p
+
+#Save Plot
+ggsave(plot = p, filename = "results/img/first_plot.png")
