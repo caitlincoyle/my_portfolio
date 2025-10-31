e4ae42b3ef02ce6d4d2e3be00aa83188b7187b7c
caitlincoyle <coyle1122@icloud.com>
2025-10-31 09:59:53 -0400

    fixing the NA from cleaning data

--- scripts/01_processing/data_processing.r
+++ scripts/01_processing/data_processing.r
@@ -17,10 +17,11 @@ colnames(ices_bycatch) #view column names
 ## Create new object to filter for taxon of turtles only
 turtle_bycatch <- ices_bycatch |>
   filter(taxon == "Turtles") |>
-  mutate(total_bycatch = multiannual_bycatch_2017_2023 + bycatch_2023) |>
+  mutate(total_bycatch = multiannual_bycatch_2017_2023 + replace_na(bycatch_2023, 0)) |>
   select(-bycatch_upper_ci, -bycatch_lower_ci, -bpue, -bpue_lower_ci, -bpue_upper_ci,
          -monitoring_effort_da_s_2017_2023, -fishing_effort_da_s_2023) #get rid of columns that make data messy
+view(turtle_bycatch)
 
-
-
+write_rds(x = turtle_bycatch,
+          file = "data/processed/turtle_bycatch_clean.rds")
 
